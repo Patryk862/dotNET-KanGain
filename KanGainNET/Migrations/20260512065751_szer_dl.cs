e497f739442831491d96c6fb8b040b439a37e362
Szymon <golden04m@wp.pl>
2026-05-12 09:01:03 +0200

    dane do tabeli

---
+++ KanGainNET/Migrations/20260512065751_szer_dl.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace KanGainNET.Migrations
+{
+    /// <inheritdoc />
+    public partial class szer_dl : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "Dlugosc",
+                table: "Lokalizacje",
+                type: "decimal(18,2)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "Szerokosc",
+                table: "Lokalizacje",
+                type: "decimal(18,2)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Dlugosc",
+                table: "Lokalizacje");
+
+            migrationBuilder.DropColumn(
+                name: "Szerokosc",
+                table: "Lokalizacje");
+        }
+    }
+}
