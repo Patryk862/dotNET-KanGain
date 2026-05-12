683dd324a7f2adce4e23e6d17f3b5557ced2431a
Patryk862 <kyrtap862@gmail.com>
2026-05-12 18:23:59 +0200

    Poprawki poprawek

--- KanGainNET/Models/Data/LokalizacjaModel.cs
+++ KanGainNET/Models/Data/LokalizacjaModel.cs
@@ -1,3 +1,5 @@
+using Microsoft.EntityFrameworkCore;
+
 namespace KanGainNET.Models
 {
     public class Lokalizacja {
@@ -5,8 +7,10 @@ namespace KanGainNET.Models
         public string Nazwa { get; set; }
         public string Adres { get; set; }
         public string Miasto { get; set; }
-        public double Dlugosc { get; set; }
-        public double Szerokosc { get; set; }
+        [Precision(12, 8)]
+        public decimal Dlugosc { get; set; }
+        [Precision(12, 8)]
+        public decimal Szerokosc { get; set; }
 
         public virtual ICollection<Sala> Sale { get; set; }
         public virtual ICollection<Sprzet> Sprzety { get; set; }
