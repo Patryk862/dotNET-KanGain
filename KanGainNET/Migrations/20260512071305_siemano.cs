33aded54345c08b40efa54a96eea528a77a11392
Szymon <golden04m@wp.pl>
2026-05-12 09:13:23 +0200

    poprawki do tabeli

---
+++ KanGainNET/Migrations/20260512071305_siemano.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace KanGainNET.Migrations
+{
+    /// <inheritdoc />
+    public partial class siemano : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Szerokosc",
+                table: "Lokalizacje",
+                type: "decimal(12,8)",
+                nullable: true,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Dlugosc",
+                table: "Lokalizacje",
+                type: "decimal(12,8)",
+                nullable: true,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)",
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Szerokosc",
+                table: "Lokalizacje",
+                type: "decimal(18,2)",
+                nullable: true,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(12,8)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Dlugosc",
+                table: "Lokalizacje",
+                type: "decimal(18,2)",
+                nullable: true,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(12,8)",
+                oldNullable: true);
+        }
+    }
+}
--- KanGainNET/Models/Data/LokalizacjaModel.cs
+++ KanGainNET/Models/Data/LokalizacjaModel.cs
@@ -1,3 +1,5 @@
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace KanGainNET.Models
 {
     public class Lokalizacja {
@@ -5,7 +7,9 @@ namespace KanGainNET.Models
         public string Nazwa { get; set; }
         public string Adres { get; set; }
         public string Miasto { get; set; }
+        [Column(TypeName = "decimal(12, 8)")]
         public decimal? Szerokosc { get; set; }
+        [Column(TypeName = "decimal(12, 8)")]
         public decimal? Dlugosc { get; set; }
 
         public virtual ICollection<Sala> Sale { get; set; }
